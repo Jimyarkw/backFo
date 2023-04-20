fd0ff43285be83f5c5bad7effe45a3c69d09a3fd
Jimyarkw <107247934+Jimyarkw@users.noreply.github.com>
2023-04-20 01:13:58 +0000

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,16 +21,14 @@ vmlink=$(echo -e '\x76\x6d\x65\x73\x73')://$(echo -n "{\"v\":\"2\",\"ps\":\"Argo
 vllink=$(echo -e '\x76\x6c\x65\x73\x73')"://8192f723-6b17-4edc-a109-ff21cdec461b@"$argo_url":443?encryption=none&security=tls&type=ws&host="$argo_url"&path=/vless?ed=2048#Argo_xray_vless"
 trlink=$(echo -e '\x74\x72\x6f\x6a\x61\x6e')"://8192f723-6b17-4edc-a109-ff21cdec461b@"$argo_url":443?security=tls&type=ws&host="$argo_url"&path=/trojan?ed2048#Argo_xray_trojan"
 
-qrencode -o /usr/share/nginx/html/M8192f723-6b17-4edc-a109-ff21cdec461b.png $vmlink
-qrencode -o /usr/share/nginx/html/L8192f723-6b17-4edc-a109-ff21cdec461b.png $vllink
-qrencode -o /usr/share/nginx/html/T8192f723-6b17-4edc-a109-ff21cdec461b.png $trlink
+
 
 cat > /usr/share/nginx/html/8192f723-6b17-4edc-a109-ff21cdec461b.html<<-EOF
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8" />
-    <title>Argo-xray-paas</title>
+    <title>Argoo</title>
     <style type="text/css">
         body {
             font-family: Geneva, Arial, Helvetica, san-serif;
@@ -47,41 +45,9 @@ cat > /usr/share/nginx/html/8192f723-6b17-4edc-a109-ff21cdec461b.html<<-EOF
     </style>
 </head>
 <body bgcolor="#FFFFFF" text="#000000">
-    <div>
-        <font color="#009900"><b>VMESS协议链接：</b></font>
-    </div>
+
     <div>$vmlink</div>
-    <div>
-        <font color="#009900"><b>VMESS协议二维码：</b></font>
-    </div>
-    <div><img src="/M8192f723-6b17-4edc-a109-ff21cdec461b.png"></div>
-    <div>
-        <font color="#009900"><b>VLESS协议链接：</b></font>
-    </div>
-    <div>$vllink</div>
-    <div>
-        <font color="#009900"><b>VLESS协议二维码：</b></font>
-    </div>
-    <div><img src="/L8192f723-6b17-4edc-a109-ff21cdec461b.png"></div>
-    <div>
-        <font color="#009900"><b>TROJAN协议链接：</b></font>
-    </div>
-    <div>$trlink</div>
-    <div>
-        <font color="#009900"><b>TROJAN协议二维码：</b></font>
-    </div>
-    <div><img src="/T8192f723-6b17-4edc-a109-ff21cdec461b.png"></div>
-    <div>
-        <font color="#009900"><b>SS协议明文：</b></font>
-    </div>
-    <div>服务器地址：$argo_url</div>
-    <div>端口：443</div>
-    <div>密码：8192f723-6b17-4edc-a109-ff21cdec461b</div>
-    <div>加密方式：chacha20-ietf-poly1305</div>
-    <div>传输协议：ws</div>
-    <div>host：$argo_url</div>
-    <div>path路径：/ssss?ed=2048</div>
-    <div>TLS：开启</div>
+    
 </body>
 </html>
 EOF
